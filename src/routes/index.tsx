721c283645200ce8b0caf524c1bd7acf5f711c1a
Raj <raj.kv@relyonsoft.com>
2025-12-30 16:07:30 +0530

    chnages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@rgba/ui/components/button'
+import { H4 } from '@rgba/ui/components/typography'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: App })
@@ -6,7 +7,7 @@ export const Route = createFileRoute('/')({ component: App })
 function App() {
   return (
     <div className="min-h-screen flex flex-col gap-4 items-center justify-center">
-      <div className="font-medium">Hello World</div>
+      <H4>Hello World</H4>
       <Button>Click Me</Button>
     </div>
   )
